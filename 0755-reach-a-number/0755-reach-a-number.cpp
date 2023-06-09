4f109230b1ea533d0a5ef3c82bb6a30a7f307afa
Aswin Shailajan <72661784+aswin2108@users.noreply.github.com>
2023-06-09 20:43:03 +0530

    Time: 4 ms (45.82%), Space: 6.1 MB (12.69%) - LeetHub

---
+++ 0755-reach-a-number/0755-reach-a-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        target=abs(target);
+        int sum=0, steps=0;
+        while(sum<target){
+            sum+=steps;
+            steps++;
+        }
+        while((sum-target)%2==1){
+            sum+=steps;
+            steps++;
+        }
+        return steps-1;
+    }
+};
