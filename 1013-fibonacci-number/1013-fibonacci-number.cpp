41096437d8f513de4f829a0804877d73e9b73657
Aswin Shailajan <72661784+aswin2108@users.noreply.github.com>
2023-06-23 10:04:57 +0530

    Time: 3 ms (68.26%), Space: 6 MB (26.06%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
-    int solve(int n, int dp[]){
-        if(n==1 || n==0)return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=fib(n-1) + fib(n-2);
-    }
 public:
     int fib(int n) {
-        int dp[n+1];
-        memset(dp, -1, sizeof dp);
-        return solve(n, dp);
+        if(n==0)return 0;
+        int prev1=1, prev2=0;
+        for(int i=2;i<=n;i++){
+            int curr=prev1 + prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
     }
 };
