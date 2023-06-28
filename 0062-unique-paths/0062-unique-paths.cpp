9b8970ea5f761c8e0a1cc8f053d52bfe385c7928
Aswin Shailajan <72661784+aswin2108@users.noreply.github.com>
2023-06-28 14:02:09 +0530

    Time: 0 ms (100%), Space: 6 MB (86.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -11,7 +11,17 @@ class Solution {
 public:
    int cnt=0;
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m, vector<int>(n, -1));
-        return path(m-1,n-1,dp);
+        int dp[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0)dp[i][j]=1;
+                else{
+                    int up=dp[i-1][j];
+                    int left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
