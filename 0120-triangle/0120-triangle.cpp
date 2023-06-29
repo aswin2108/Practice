d9b89cb8688c1131e5569b66a25dc03e781f8513
Aswin Shailajan <72661784+aswin2108@users.noreply.github.com>
2023-06-29 12:57:51 +0530

    Time: 4 ms (86.59%), Space: 8.9 MB (49.47%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -13,6 +13,19 @@ public:
         int n=triangle.size();
         int m=triangle[n-1].size();
         vector<vector<int>>dp(n, vector<int>(m, -1));
-        return solve(triangle, 0, 0, dp);
+        // return solve(triangle, 0, 0, dp);
+
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle[n-1][j];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=triangle[i][j]+dp[i+1][j];
+                int dag=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down, dag);
+            }
+        }
+        return dp[0][0];
     }
 };
