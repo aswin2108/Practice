dbd8a59617c7a03459e6903bda48879cbfd60722
Aswin Shailajan <72661784+aswin2108@users.noreply.github.com>
2023-08-11 14:29:23 +0530

    Time: 12 ms (81.17%), Space: 18.7 MB (29.68%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
-    int solve(int n, int amt, vector<int>&coins, vector<vector<int>>&dp){
-        if(n==0){
+    int solve(int idx, int amt, vector<int>&coins, vector<vector<int>>&dp){
+        if(idx==0){
             return (amt%coins[0]==0);
         }
-        if(dp[n][amt]!=-1)return dp[n][amt];
-        int notPick = solve(n-1, amt, coins, dp);
+        if(dp[idx][amt]!=-1)return dp[idx][amt];
+        int notPick=solve(idx-1, amt, coins, dp);
         int pick=0;
-        if(coins[n]<=amt){
-            pick=solve(n, amt-coins[n], coins, dp);
+        if(coins[idx]<=amt){
+            pick=solve(idx, amt-coins[idx], coins, dp);
         }
-        return dp[n][amt] = pick+notPick;
+        return dp[idx][amt] = pick+notPick;
     }
 public:
     int change(int amount, vector<int>& coins) {
