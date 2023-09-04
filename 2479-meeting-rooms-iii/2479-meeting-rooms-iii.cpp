9cb9d6c21c6afc19bb59878072c478f2e8a9a4b8
Aswin Shailajan <72661784+aswin2108@users.noreply.github.com>
2023-09-04 13:54:43 +0530

    Time: 534 ms (54.91%), Space: 110.1 MB (45.02%) - LeetHub

--- 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/2479-meeting-rooms-iii.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        sort(meetings.begin(), meetings.end());
-        vector<long long>time(n, 0);
+        vector<long long>time(n+1, 0);
         vector<int>ans(n, 0);
+        sort(meetings.begin(), meetings.end());
 
         for(auto it:meetings){
-            int start=it[0];
-            int end=it[1];
-            bool flag=false;
+            int start = it[0];
+            int end = it[1];
             int minIdx=-1;
             long long minVal=1e18;
+            bool flag = false;
+
             for(int i=0;i<n;i++){
-                if(time[i]<minVal)minVal=time[i], minIdx=i;
-                if(time[i]<=start){
+                if(time[i] < minVal){
+                    minVal=time[i];
+                    minIdx=i;
+                }
+                if(time[i] <= start){
                     time[i]=end;
                     flag=true;
                     ans[i]++;
                     break;
                 }
             }
+
             if(!flag){
-                time[minIdx]+=(1LL*(end-start));
+                time[minIdx] += (1LL* (end-start));
                 ans[minIdx]++;
             }
         }
-        int maxFreq=-1, maxFreqRoom=-1;
+        
+        int maxFreq = -1, maxFreqRoom = -1;
         for(int i=0;i<n;i++){
-            if(ans[i]>maxFreq)maxFreq=ans[i],maxFreqRoom=i;
+            if(maxFreq < ans[i]){
+                maxFreq = ans[i];
+                maxFreqRoom = i;
+            }
         }
         return maxFreqRoom;
     }
