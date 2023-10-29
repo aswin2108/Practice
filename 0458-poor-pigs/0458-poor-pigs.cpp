f28a7f326796cc2fe4fddec8320621ca01ab2dad
Aswin Shailajan <72661784+aswin2108@users.noreply.github.com>
2023-10-30 00:13:09 +0530

    Time: 0 ms (100%), Space: 6.6 MB (8.64%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+
+int poorPigs(int buckets, int poisonTime, int totalTime) {
+    int pig = 0;
+        while(pow(((totalTime/poisonTime) +1),pig) < buckets ) pig++;
+        return pig;
+}
+};
